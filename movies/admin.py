8b0d81fa61ede323ca89fca748016d3e608470d3
Valera <valera@gmail.com>
2022-12-06 02:39:39 +0200

    add django actions and ckeditor

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,19 @@
+from django import forms
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 from .models import Category, Genre, Movie, MovieShorts, Actor, RatingStar, Rating, Reviews
+
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label='Описание', widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
@@ -40,9 +52,11 @@ class MovieAdmin(admin.ModelAdmin):
     list_editable = ('draft',)
     readonly_fields = ("get_photo",)
     inlines = [MovieShotsInline, ReviewsInline]
+    actions = ['publish', 'unpublish']
     prepopulated_fields = {'url': ('title',)}
     save_on_top = True
     save_as = True
+    form = MovieAdminForm
     # fields = (('actors', 'directors', 'genres'),)
     fieldsets = (
         (None, {
@@ -68,6 +82,32 @@ class MovieAdmin(admin.ModelAdmin):
 
     def get_photo(self, obj):
         return mark_safe(f'<img src="{obj.poster.url}" width="110" height="110">')
+
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+
+        if row_update == 1:
+            message_bit = '1 запись была обновлена'
+        else:
+            message_bit = f'{row_update} записей были обновлены'
+        self.message_user(request, f'{message_bit}')
+
+    publish.short_description = 'Опубликовать'
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = 'Снять с публикации'
+    unpublish.allowed_permissions = ('change',)
 
     get_photo.short_description = 'Постер'
 
